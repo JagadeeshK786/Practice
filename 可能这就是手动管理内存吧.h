76f13e48732a74c36cb320c630d55941c676e2bb
liuziangexit <liuziang@liuziangexit.com>
2017-07-18 18:16:25 +0800

    开始实现1

--- 可能这就是手动管理内存吧.h
+++ 可能这就是手动管理内存吧.h
@@ -1,11 +1,150 @@
 #pragma once
 #include <stdlib.h>
 #include <memory.h>
+#include "DC_Exception.h"
+#include "DC_STR.h"
 
 namespace DC {
 	using byte_t = unsigned char;
 	using size_t = unsigned int;
 }
+
+class SpinLock final {
+public:
+	SpinLock() :flag{ 0 } {}
+
+	SpinLock(const SpinLock&) = delete;
+
+public:
+	void lock() {
+		while (flag.test_and_set(std::memory_order::memory_order_acquire));
+	}
+
+	bool try_lock() {
+		return !flag.test_and_set(std::memory_order_acquire);
+	}
+
+	void unlock() {
+		flag.clear(std::memory_order::memory_order_release);
+	}
+
+private:
+	std::atomic_flag flag;
+};
+
+namespace LockFreeLinkedListSpace {
+
+	template <typename atomic_t>
+	inline auto load_acquire(const atomic_t& _Th)noexcept->decltype(_Th.load(std::memory_order::memory_order_acquire)) {
+		return _Th.load(std::memory_order::memory_order_acquire);
+	}
+
+	template <typename atomic_t, typename value_t>
+	inline void store_release(atomic_t& _Th, value_t&& _Val)noexcept {
+		_Th.store(std::forward<value_t>(_Val), std::memory_order::memory_order_release);
+	}
+
+	template <typename _Ty>
+	class node {
+	public:
+		node() : object(nullptr), next_ptr(nullptr) {}
+
+		template <typename ...ARGS>
+		node(ARGS&& ...args) : object(new _Ty(std::forward<ARGS>(args)...)), next_ptr(nullptr) {}
+
+		~node() {
+			if (object)
+				delete object;
+		}
+
+	public:
+		inline node<_Ty>* load_next()const {
+			return load_acquire(next_ptr);
+		}
+
+		inline void store_next(node<_Ty>* new_ptr) {
+			store_release(next_ptr, new_ptr);
+		}
+
+	public:
+		_Ty* object;
+		SpinLock sl;
+
+	private:
+		std::atomic<node<_Ty>*> next_ptr;
+	};
+
+}
+
+template <typename _Ty>
+class LockFreeLinkedList final {
+public:
+	using value_type = _Ty;
+	using size_type = DC::size_t;
+
+public:
+	LockFreeLinkedList() :m_head(nullptr) {
+		//debug
+		m_head.store(new LockFreeLinkedListSpace::node<value_type>("9710"));
+		for (int i = 0; i < 3; i++) {
+			LockFreeLinkedListSpace::node<value_type>* it = m_head.load();
+			while (1) {
+				if (it->load_next() == nullptr) {
+					it->store_next(new LockFreeLinkedListSpace::node<value_type>(DC::STR::toString(i + 1)));
+					break;
+				}
+				it = it->load_next();
+			 }
+		}
+	}
+
+	~LockFreeLinkedList() {
+		clear();
+	}
+
+public:
+	void insert(const value_type&) {}
+
+	template <typename ...ARGS>
+	void emplace(ARGS&& ...args) {}
+
+	bool erase(const value_type&) {}
+
+	void clear() {
+
+	}
+
+	template <typename _Pr>
+	value_type& find(const _Pr& _Pred) {//_Pr是一个可调用对象，应该接受_Pr(const value_type&)方式的调用，如果找到了，返回true，否则返回false
+		if (LockFreeLinkedListSpace::load_acquire(m_head) == nullptr)
+			throw DC::Exception("LockFreeLinkedList::find", "object not found");
+
+		for (LockFreeLinkedListSpace::node<value_type>* ptr(LockFreeLinkedListSpace::load_acquire(m_head)); ; ptr = ptr->load_next()) {
+			if (_Pred(*ptr->object)) return *ptr->object;
+			if (ptr->load_next() == nullptr) break;
+		}
+
+		throw DC::Exception("LockFreeLinkedList::find", "object not found");
+	}
+
+private:
+	template <typename _Pr>
+	LockFreeLinkedListSpace::node<value_type>* internal_find(const _Pr& _Pred)noexcept {
+		if (LockFreeLinkedListSpace::load_acquire(m_head) == nullptr)
+			return nullptr;
+
+		for (LockFreeLinkedListSpace::node<value_type>* ptr(LockFreeLinkedListSpace::load_acquire(m_head)); ; ptr = ptr->load_next()) {
+			if (_Pred(ptr)) return ptr;
+			if (ptr->load_next() == nullptr) break;
+		}
+
+		return nullptr;
+	}
+
+private:
+	std::atomic<LockFreeLinkedListSpace::node<value_type>*> m_head;
+	SpinLock m_head_lock;
+};
 
 template <typename _Ty>
 class MemoryPool final {
