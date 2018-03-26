c980f033c88f055fc731e54f8ab66b3c76739953
liuziangexit <liuziang@liuziangexit.com>
2018-03-26 19:25:36 +0800

    xxx

---
+++ 塞尔达录入/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+//需要确保文件为 UTF8 with BOM 编码
+
+namespace ZeldaData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string md, content, type;
+            Console.Write("输入md文件名:");
+            //md = Console.ReadLine();
+            md = "E:\\OneDrive\\杂项\\Zelda塞尔达项目\\md\\Weapons盾.md";
+            content = File.ReadAllText(md, Encoding.UTF8);
+            type = content.Substring(content.IndexOf('#', 0) + 2, content.IndexOf('\n', 0) - 2 - content.IndexOf('#', 0));
+            var bgfrs = Util.search_all(content, "##");
+            var edfrs = Util.search_all(content, "\n\n");
+            var items = Util.get_items_from_substrings(Util.get_item_substrings(content, bgfrs, edfrs));
+            var look = "{\r\n  \"content\":" + JsonConvert.SerializeObject(items, Formatting.Indented) + "\r\n}";
+            File.WriteAllText("E:\\OneDrive\\杂项\\Zelda塞尔达项目\\Weapons.json", look);
+        }
+    }
+}
---
+++ 塞尔达录入/Util.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZeldaData
+{
+    class Util
+    {
+        static public string image_weapons_url = "https://starryedge.com/ZeldaPS/Data/Image/Weapons/",
+            page_weapons_url = "https://starryedge.com/ZeldaPS/Data/Page/Weapons/";
+
+        public static List<int> search_all(string str, string findme)
+        {
+            List<int> rv = new List<int>();
+            int pos = str.IndexOf(findme, 0);
+            while (pos != -1)
+            {
+                rv.Add(pos);
+                pos = str.IndexOf(findme, pos + 1);
+            }
+            return rv;
+        }
+
+        public static int search_time(string str, string findme, int times)
+        {
+            int pos = -1;
+            for (int i = 0; i < times; i++)
+            {
+                pos = str.IndexOf(findme, pos + 1);
+                if (pos == -1)
+                    break;
+            }
+            return pos;
+        }
+
+        public static List<string> get_item_substrings(string str, List<int> bglist, List<int> edlist)
+        {
+            if (bglist.Count != edlist.Count)
+                throw new Exception();
+            List<string> substrings = new List<string>();
+            for (int i = 0; i < bglist.Count; i++)
+                substrings.Add(str.Substring(bglist[i], edlist[i] - bglist[i]));
+            return substrings;
+        }
+
+        public static List<item> get_items_from_substrings(List<string> substrings)
+        {
+            List<item> rv = new List<item>();
+            foreach (string p in substrings)
+            {
+                item it = new item();
+                it.ChineseName = p.Substring(search_time(p, " ", 2), p.IndexOf('!') - search_time(p, " ", 2));
+                rv.Add(it);
+                it.number = p.Substring(p.IndexOf(' ') + 1, search_time(p, " ", 2) - p.IndexOf(' ') - 1);
+                it.Image = image_weapons_url + it.number.Replace('/', '_') + ".png";
+                it.ChineseIntro = page_weapons_url + it.number.Replace('/', '_');
+            }
+            return rv;
+        }
+
+        public static string toBase64(string str)
+        {
+            byte[] temp = System.Text.Encoding.Default.GetBytes(str);
+            return Convert.ToBase64String(temp);
+        }
+
+        public static string fromBase64(string str)
+        {
+            byte[] temp = Convert.FromBase64String(str);
+            return System.Text.Encoding.Default.GetString(temp);
+        }
+
+    }
+
+    class item
+    {
+        public string ChineseName
+        {
+            get { return Util.fromBase64(chinesename); }
+            set { chinesename = Util.toBase64(value); }
+        }
+        private string chinesename;
+        public string Image { get; set; }
+        public string ChineseIntro { get; set; }
+        public string number { get; set; }
+    }
+}
