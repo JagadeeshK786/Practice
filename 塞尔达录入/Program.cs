b1a3c9f9f27e03249ef5563227f4c09341b99637
liuziangexit <liuziang@liuziangexit.com>
2018-04-05 17:27:26 +0800

    xx

--- 塞尔达录入/Program.cs
+++ 塞尔达录入/Program.cs
@@ -14,14 +14,30 @@ namespace ZeldaData
             string md, content, type;
             Console.Write("输入md文件名:");
             //md = Console.ReadLine();
-            md = "E:\\OneDrive\\杂项\\Zelda塞尔达项目\\md\\Creatures.md";
+            md = "E:\\OneDrive\\杂项\\Zelda塞尔达项目\\md\\Weapons-盾.md";           
             content = File.ReadAllText(md, Encoding.UTF8);
+
+            bool ShouldClean = false;
+            //bool ShouldClean = true;
+            if (ShouldClean)
+            {
+                int pos = content.IndexOf("![", 0);
+                while (pos != -1)
+                {
+                    content = content.Remove(pos - 1, 1);
+                    pos--;
+                    pos = content.IndexOf("\n\n", pos);
+                    content = content.Remove(pos, 1);
+                    pos = content.IndexOf("![", pos);
+                }
+            }
+
             type = content.Substring(content.IndexOf('#', 0) + 2, content.IndexOf('\n', 0) - 2 - content.IndexOf('#', 0));
             var bgfrs = Util.search_all(content, "##");
             var edfrs = Util.search_all(content, "\n\n");
             var items = Util.get_items_from_substrings(Util.get_item_substrings(content, bgfrs, edfrs));
             var look = JsonConvert.SerializeObject(items, Formatting.Indented);
-            File.WriteAllText("E:\\OneDrive\\杂项\\Zelda塞尔达项目\\Creatures生物.json", look, Encoding.UTF8);
+            File.WriteAllText("E:\\OneDrive\\杂项\\Zelda塞尔达项目\\Weapons-盾.json", look, Encoding.UTF8);
         }
     }
 }
