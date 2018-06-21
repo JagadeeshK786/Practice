7507a1b1ba59216b1dc7e1cd0639d9d9e1724055
liuziangexit <liuziang@liuziangexit.com>
2018-06-21 21:14:34 +0800

    xxx

---
+++ CONST_LAUNDER_TEST.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+struct X { const int n; };
+union U { X x; float f; };
+
+int main() {
+	U u = { { 1 } };
+	X *p = new (&u.x) X{ 9710 }; //在 u.x 的位置上重新构造一个 X
+	std::cout << u.x.n;
+}
