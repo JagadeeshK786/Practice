262cdd2ead0b54867412338c28eda0d208f13120
liuziangexit <liuziang@liuziangexit.com>
2018-06-21 22:20:16 +0800

    update

--- CONST_LAUNDER_TEST.cpp
+++ CONST_LAUNDER_TEST.cpp
@@ -7,7 +7,10 @@ struct X {
 
 int main() {
     X* p = new X{7, 8.8};
-    X* new_p = new(p) X{9710, 6.6};
+    X* new_p = new(p) X{9710, 6.6}; //在p的位置构造一个新值
+	int b = p->n;
+	int c = p->n;
+	double d = p->d;
     std::cout<< "old pointer:" << p->n << "  " << p->d << "\n";
     std::cout<< "new pointer:" << new_p->n << "  " << new_p->d << "\n";
 }
