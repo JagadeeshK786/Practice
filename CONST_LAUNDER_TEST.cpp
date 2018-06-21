458e021e76032c617922bad54bba43de05ede958
liuziangexit <liuziang@liuziangexit.com>
2018-06-21 22:21:48 +0800

    xx

--- CONST_LAUNDER_TEST.cpp
+++ CONST_LAUNDER_TEST.cpp
@@ -1,12 +1,12 @@
 ﻿#include <iostream>
 
 struct X {
-    const int n;
-    const double d;
+	const int n;
+	const double d;
 };
 
 int main() {
-    X* p = new X{7, 8.8};
+	X* p = new X{7, 8.8};
 	new_p = new(p) X{9710, 6.6}; //在p的位置构造一个新值
 	int b = p->n;
 	int c = p->n;
