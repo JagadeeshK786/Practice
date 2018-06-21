1f3896873f08a2b066e1ff0adb6aa88a57c4bbe7
liuziangexit <liuziang@liuziangexit.com>
2018-06-21 21:18:23 +0800

    更新注释

--- CONST_LAUNDER_TEST.cpp
+++ CONST_LAUNDER_TEST.cpp
@@ -4,7 +4,7 @@ struct X { const int n; };
 union U { X x; float f; };
 
 int main() {
-	U u = { { 1 } };
+	U u = { { 1 } }; //聚合初始化一个U，它的活跃成员是x，x.n为1
 	X *p = new (&u.x) X{ 9710 }; //在 u.x 的位置上重新构造一个 X
 	std::cout << u.x.n;
 }
