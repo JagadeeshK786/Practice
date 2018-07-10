9f009b6140016d7c9e21be267af9fc9479f757c9
liuziangexit <liuziang@liuziangexit.com>
2018-07-11 00:44:48 +0800

    zzz

--- 有机会一起倒计时.cpp
+++ 有机会一起倒计时.cpp
@@ -3,34 +3,34 @@
 #include <conio.h>
 
 int main() {
-	while (true) {
-		auto now = time(0);
-
-		tm target{ 0 };
-		target.tm_year = 118;
-		target.tm_mon = 6;
-		target.tm_mday = 26;
-		target.tm_hour = 21;
-		target.tm_min = 0;
-		target.tm_sec = 0;
-
-		auto distance = mktime(&target);
-
-		if (distance > now) {
-			distance -= now;
-		}
-		else {
-			std::cout << "2018.7.26 9:00 pm ÒÑ¾­¹ýÈ¥¡£";
-			_getch();
-			std::cout << "\n";
-		}
-
-		target = *gmtime(&distance);
-
-		std::cout << target.tm_yday << "Ìì/" << (((double)365 - (double)target.tm_yday) / (double)365) * 100 << "%";
-		_getch();
-		std::cout << "\n";
-	}
-
-	return 0;
+    while (true) {
+        auto now = time(0);
+        
+        tm target{ 0 };
+        target.tm_year = 118;
+        target.tm_mon = 6;
+        target.tm_mday = 26;
+        target.tm_hour = 21;
+        target.tm_min = 0;
+        target.tm_sec = 0;
+        
+        auto distance = mktime(&target);
+        
+        if (distance > now) {
+            distance -= now;
+        }
+        else {
+            std::cout << "2018.7.26 9:00 pm å·²ç»è¿‡åŽ»";
+            _getch();
+            std::cout << "\n";
+        }
+        
+        target = *gmtime(&distance);
+        
+        std::cout << target.tm_yday << "å¤©/" << (((double)365 - (double)target.tm_yday) / (double)365) * 100 << "%";
+        _getch();
+        std::cout << "\n";
+    }
+    
+    return 0;
 }
