ef971433ec610ab9a19e64fc8ba7ecbb74450737
liuziangexit <liuziang@liuziangexit.com>
2018-08-29 20:50:03 +0800

    xxxsd

---
+++ 数据结构和算法/README.md
@@ -0,0 +1,2 @@
+# 数据结构和算法练习
+https://github.com/julycoding/The-Art-Of-Programming-By-July
---
+++ 数据结构和算法/源.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+void first2end(char* str, unsigned int size) {
+	if (size < 2)
+		return;
+	char first = str[0];
+	for (int i = 1; i < size; i++)
+		str[i - 1] = str[i];
+	str[size - 1] = first;
+}
+
+void some2end(char *str , unsigned int size, int count) {
+	while (count--)
+		first2end(str, size);
+}
+
+int main() {
+	char hi[] = "123456";
+	some2end(hi, 6, 2);
+	std::cout << "";
+}
