e86cf4391265eba2fa389d23d21e6319367ecc66
liuziangexit <liuziang@liuziangexit.com>
2018-08-31 09:58:21 +0800

    不错昂，提交一哈

--- 数据结构和算法/源.cpp
+++ 数据结构和算法/源.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 #include <string>
 #include <unordered_set>
-#include <DC_File.h>
+#include <cctype>
+#include <cmath>
 
 //https://leetcode-cn.com/problems/two-sum/description/
 std::vector<int> twoSum(const std::vector<int>& nums, int target) {
@@ -13,58 +14,81 @@ std::vector<int> twoSum(const std::vector<int>& nums, int target) {
 }
 
 //https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/description/
-//不要一次性拿全部子串，按个数拿子
-std::vector<std::string> getAllSubstring(const std::string& str) {
-	std::unordered_set<std::size_t> hash_value_set;
-	std::hash<std::string> string_hash;
-	std::vector<std::string> returnMe;
-	for (std::size_t i = str.size(); i > 0; i--) {
-		for (std::size_t i2 = 0; i2 <= str.size() - i; i2++) {
-			auto tmp = str.substr(i2, i);
-			auto tmp_hash = string_hash(tmp);
-			if (hash_value_set.find(tmp_hash) == hash_value_set.end()) {
-				returnMe.push_back(std::move(tmp));
-				hash_value_set.insert(tmp_hash);
+int lengthOfLongestSubstring(const std::string& s) {
+	std::unordered_set<char> set;
+	unsigned int returnme = 0, i = 0, j = 0;
+	while (i < s.size() && j < s.size()) {
+		for (j = i; j < s.size(); j++) {
+			if (set.find(s[j]) == set.end()) {
+				if (j - i + 1 > returnme)
+					returnme = j - i + 1;
+				set.insert(s[j]);
+			}
+			else {
+				break;
 			}
 		}
+		set.clear();
+		i++;
 	}
-	return returnMe;
+	return returnme;
 }
 
-int lengthOfLongestSubstring(const std::string& s) {
-	std::unordered_set<std::size_t> hash_value_set;
-	std::hash<std::string> string_hash;
-	int returnMe = 0;
-	for (std::size_t i = s.size(); i > 0; i--) {
-		for (std::size_t i2 = 0; i2 <= s.size() - i; i2++) {
-			auto tmp = s.substr(i2, i);
-			auto tmp_hash = string_hash(tmp);
-			if (hash_value_set.find(tmp_hash) == hash_value_set.end()) {
-				const auto& p = tmp;				
-				std::unordered_set<char> set;
-				if (p.size() <= returnMe)
-					continue;
-				bool isOK = true;
-				for (const auto& c : p) {
-					if (set.find(c) != set.end()) {
-						isOK = false;
-						break;
-					}
-					else {
-						set.insert(c);
-					}
-				}
-				if (isOK)
-					returnMe = p.size();
-				hash_value_set.insert(tmp_hash);
-			}
-		}
+//https://leetcode-cn.com/problems/string-to-integer-atoi/description/
+static const int32_t int_max = 2147483647;
+static const int32_t int_min = -2147483648;
+
+int32_t get_digit_count(int32_t num) {
+	if (num == int_min)
+		return 10;
+	return static_cast<int32_t>(log10(abs(num))) + 1;
+}
+
+int32_t get_each_digit(int32_t x) {
+	if (x >= 10)
+		return get_each_digit(x / 10);
+	return x % 10;
+}
+
+int myAtoi(const std::string& str) {
+	//获得有效数字范围
+	unsigned int begin = 0, end = 0;
+	bool is_positive = true;
+	for (; begin < str.size(); begin++)
+		if (!std::isspace(str[begin]))
+			break;
+	if (str.empty() || std::isspace(str[begin])
+		|| (!std::isdigit(str[begin]) && str[begin] != '-'))
+		return 0;
+	for (end = begin + 1; end < str.size(); end++)
+		if (!std::isdigit(str[end]))
+			break;
+	if (end - begin == 1)
+		if (str[begin] == '-')
+			return 0;
+	if (str[begin] == '-'&&end - begin > 1) {
+		is_positive = false;
+		begin++;
 	}
-	return returnMe;
+	//判断是否超过32位有符号限制
+	if (end - begin > 10) {
+		if (is_positive)
+			return int_max;
+		else
+			return int_min;
+	}
+	if (end - begin == 10) {
+		std::cout << "";
+	}
+	int32_t result = 0;
+	for (; begin != end; begin++)
+		result = result * 10 + str[begin] - '0';
+	if (!is_positive)
+		result = ~result + 1;
+	return result;
 }
 
 int main() {
-	auto look2 = lengthOfLongestSubstring("abcabcbb");
-	auto look = lengthOfLongestSubstring(DC::File::read("1.txt"));
+	auto look = myAtoi("-9710");
 	std::cout << "";
 }
