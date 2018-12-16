ea321c433f6701b41c6670d3e6e4fb88ae9157a5
liuziangexit <liuziang@liuziangexit.com>
2018-12-16 19:00:24 +0800

    Update insertion_sort.cpp

--- 算法导论/insertion_sort.cpp
+++ 算法导论/insertion_sort.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <utility>
 #include <vector>
 
 template <typename IterType, typename PredType>
@@ -18,9 +17,32 @@ void insertion_sort(IterType begin, IterType end, const PredType &pred)
     }
 }
 
+template <typename IterType, typename PredType>
+void merge_sort(IterType begin1, IterType end1, IterType begin2, IterType end2, IterType output, const PredType &pred)
+{
+    while (begin1 != end1 && begin2 != end2)
+    {
+        if (pred(*begin1, *begin2) > 0)
+            *output++ = *begin1++;
+        else
+            *output++ = *begin2++;
+    }
+    auto push_back = [&output](IterType begin, IterType end) -> void {
+        while (begin != end)
+            *output++ = *begin++;
+    };
+    if (begin1 == end1)
+        push_back(begin2, end2);
+    else
+        push_back(begin1, end1);
+}
+
 int main()
 {
     std::vector<int> vec{2, 1, -1, 10, 7, 8, 9, 4, 2, 100, -10, -2};
     insertion_sort(vec.begin(), vec.end(), [](auto arg0, auto arg1) { return arg1 - arg0; });
+    
+    std::vector<int> first{1, 3, 5, 7, 9}, second{2, 4, 6, 8, 10}, output(10);
+    merge_sort(first.begin(), first.end(), second.begin(), second.end(), output.begin(), [](auto arg0, auto arg1) { return arg1 - arg0; });
     std::cout << "";
 }
