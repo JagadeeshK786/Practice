0e4f80ca7142a442979eef73b123eeea3fb45b88
liuziangexit <liuziang@liuziangexit.com>
2019-01-07 00:03:51 +0800

    Update main.cpp

--- 算法导论/main.cpp
+++ 算法导论/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <algorithm> //std::copy
+#include <cstddef>
 
 #ifndef _DEBUG
 template <typename IterType, typename PredType>
@@ -40,34 +42,57 @@ void insertion_sort(IterType begin, IterType end, const PredType &pred)
 }
 #endif
 
+#include <new>
+#include <memory>
+
 template <typename IterType, typename PredType>
-void merge_sort(IterType begin1, IterType end1, IterType begin2, IterType end2, IterType output, const PredType &pred)
+void merge_impl(IterType begin, IterType middle, IterType end, const PredType &pred)
 {
-	while (begin1 != end1 && begin2 != end2)
+	//当IterType::value_type不为trivially copyable类型时，下面的new带来了多余的初始化操作
+	std::unique_ptr<IterType::value_type[]> temp_array(new IterType::value_type[std::distance(begin, end)]);
+
+	std::copy(begin, end, temp_array.get());
+
+	std::size_t left_begin = 0, left_end = std::distance(begin, middle);
+	std::size_t right_begin = left_end, right_end = right_begin + std::distance(middle, end);
+
+	while (left_begin != left_end && right_begin != right_end)
 	{
-		if (pred(*begin1, *begin2) > 0)
-			*output++ = *begin1++;
+		if (pred(temp_array[left_begin], temp_array[right_begin]) > 0)
+			*begin++ = std::move(temp_array[left_begin++]);
 		else
-			*output++ = *begin2++;
+			*begin++ = std::move(temp_array[right_begin++]);
 	}
-	auto push_back = [&output](IterType begin, IterType end) -> void {
-		while (begin != end)
-			*output++ = *begin++;
-	};
-	if (begin1 == end1)
-		push_back(begin2, end2);
+
+	if (left_begin == left_end)
+		std::copy(&temp_array[right_begin], &temp_array[right_end], begin);
 	else
-		push_back(begin1, end1);
+		std::copy(&temp_array[left_begin], &temp_array[left_end], begin);
+}
+
+template <typename IterType, typename PredType>
+void merge_sort(IterType begin, IterType end, const PredType &pred)
+{
+	auto distance = std::distance(begin, end);
+	if (begin == end || distance == 1)
+		return;
+
+	//在数据较多时将会栈溢出
+	auto middle = distance / 2;
+	merge_sort(begin, begin + middle, pred);
+	merge_sort(begin + middle, end, pred);
+	merge_impl(begin, begin + middle, end, pred);
 }
 
 int main()
 {
-	std::vector<int> vec{ 1, 2,-1, 10, 7, 8, 9, 4, 2, 100, -10, -2 };
+	std::vector<int> vec{ 1, 2, -1, 10, 7, 8, 9, 4, 2, 100, -10, -2 };
 	insertion_sort(vec.begin(), vec.end(), [](auto arg0, auto arg1) { return arg1 - arg0; });
 
-	std::vector<int> first{ 1, 3, 5, 7, 9, 100 }, second{ 2, 4, 6, 8, 10, 100, 1000 };
-	decltype(first) output(first.size() + second.size());
-	merge_sort(first.begin(), first.end(), second.begin(), second.end(), output.begin(), [](auto arg0, auto arg1) { return arg1 - arg0; });
+	std::vector<int> vec2{ 1, 3, 5, 7, 9, 100, 2, 4, 6, 8, 10, 100, 1000 };
+	//std::vector<int> vec2{ 1,100, 9,  2 };
+	//merge_impl(vec2.begin(), vec2.begin() + 6, vec2.end(), [](auto arg0, auto arg1) { return arg1 - arg0; });
+	merge_sort(vec2.begin(), vec2.end(), [](auto arg0, auto arg1) { return arg1 - arg0; });
 
 	std::cout << "";
 }
