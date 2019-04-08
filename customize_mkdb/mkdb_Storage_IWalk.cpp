ef56def3e927303529d460b07989a1cb757f9504
liuziangexit <liuziang@liuziangexit.com>
2019-04-08 11:54:40 +0800

    update mkdb

---
+++ customize_mkdb/mkdb_Storage_IWalk.cpp
@@ -0,0 +1,12 @@
+#include "mkdb_Storage_IWalk.h"
+/* Implementation for class mkdb_Storage_IWalk */
+
+#ifndef _Included_mkdb_Storage_IWalk_Impl
+#define _Included_mkdb_Storage_IWalk_Impl
+#ifdef __cplusplus
+extern "C" {
+#endif
+#ifdef __cplusplus
+}
+#endif
+#endif
---
+++ customize_mkdb/mkdb_Storage_KeyIterator.cpp
@@ -0,0 +1,12 @@
+#include "mkdb_Storage_KeyIterator.h"
+/* Implementation for class mkdb_Storage_KeyIterator */
+
+#ifndef _Included_mkdb_Storage_KeyIterator_Impl
+#define _Included_mkdb_Storage_KeyIterator_Impl
+#ifdef __cplusplus
+extern "C" {
+#endif
+#ifdef __cplusplus
+}
+#endif
+#endif
