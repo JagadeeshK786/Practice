ffa2388d373028862078a715728a5695b187e529
liuziangexit <liuziang@liuziangexit.com>
2019-09-09 21:25:46 +0800

    试试无锁

---
+++ FixedLockFreeHashMap.java
@@ -0,0 +1,152 @@
+package fire.pb.util;
+
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReferenceArray;
+
+/**
+ * @author liuziang
+ * @contact liuziang@liuziangexit.com
+ * @date 9/9/2019
+ * @description 大小不可变的无锁Map，具有acquire-release语义（隐式获得此语义自Atomic访问)
+ * @参见 The Java® Language Specification - §17.4.4 "Synchronization Order"
+ */
+
+//TODO:删除功能、处理map满的情况
+
+public class FixedLockFreeHashMap<K, V> implements java.util.Map<K, V> {
+
+	public FixedLockFreeHashMap(int capacity) {
+		this.data = new AtomicReferenceArray<>(capacity);
+		//TODO: more cache-friendly allocation
+		for (int i = 0; i < this.data.length(); i++)
+			this.data.set(i, new InternalEntry(0, null));
+	}
+
+	@Override
+	public int size() {
+		return this.size.get();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return this.size.get() == 0;
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		throw new RuntimeException();
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public V get(Object key) {
+		int keyHash = key.hashCode();
+		for (int entryKey = keyHash % this.data.length(); ; entryKey = ++keyHash % this.data.length()) {
+			InternalEntry entry = (InternalEntry) this.data.get(entryKey);
+			if (entry.key == keyHash)
+				return (V) entry.value;//found it
+			if (entry.key == 0)
+				return null;//if there is no value associated with the key
+		}
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public V put(K key, V value) {
+		return (V) this.putImpl(new InternalEntry(key.hashCode(), value)).value;
+	}
+
+	@Override
+	public V remove(Object key) {
+		throw new RuntimeException();
+	}
+
+	@Override
+	public void putAll(java.util.Map<? extends K, ? extends V> m) {
+		m.forEach(this::put);
+	}
+
+	@Override
+	public void clear() {
+		for (int i = 0; i < this.data.length(); i++)
+			this.data.set(i, null);
+	}
+
+	@Override
+	public java.util.Set<K> keySet() {
+		throw new RuntimeException();
+	}
+
+	@Override
+	public java.util.Collection<V> values() {
+		throw new RuntimeException();
+	}
+
+	@Override
+	public java.util.Set<Entry<K, V>> entrySet() {
+		throw new RuntimeException();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		throw new RuntimeException();
+	}
+
+	@Override
+	public int hashCode() {
+		throw new RuntimeException();
+	}
+
+	private InternalEntry putImpl(InternalEntry newEntry) {
+		int hash = newEntry.key;
+		InternalEntry previousEntry = null;
+		for (int entryKey = hash % this.data.length(); previousEntry == null; entryKey = ++hash % this.data.length()) {
+			do {
+				previousEntry = (InternalEntry) this.data.get(entryKey);
+				if (previousEntry.key != entryKey && previousEntry.key != 0) {
+					//this entry has been used by other key(hash collision occurs), try next entry
+					previousEntry = null;
+					break;
+				}
+				//if this key is already associated with a value...
+				//...or this entry is empty...
+				//...try CAS
+			} while (!this.data.weakCompareAndSet(entryKey, previousEntry, newEntry));
+		}
+		return previousEntry;
+	}
+
+	static private class InternalEntry {
+
+		InternalEntry(int k, Object v) {
+			this.key = k;
+			this.value = v;
+		}
+
+		@Override
+		public boolean equals(Object rhs) {
+			if (!(rhs instanceof InternalEntry))
+				return false;
+			return this.hashCode() == rhs.hashCode();
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hashCode(key);
+		}
+
+		public int key;
+		public Object value;
+
+	}
+
+	private AtomicReferenceArray<Object> data;
+	private AtomicInteger size = new AtomicInteger(0);
+
+}
